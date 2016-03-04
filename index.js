bed5b266b8743ff44fbaa1264293d650c05d0438
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-04 11:43:56 -0800

    add index file changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var requestHandlers = require('./requestHandlers');
 var handle = {};
 handle['/'] = requestHandlers.start;
 handle['/start'] = requestHandlers.start;
-handle['/upload'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+handle['/show'] = requestHandlers.show;
 
 server.start(router.route, handle);
